15b1289572762baa6ebd30f3bbcb74d879780a2e
karenbltrnlpz <karen.bltrnlpz@gmail.com>
2024-02-15 22:53:44 -0600

    Add .gitignore files along with the rest of the files/folders

---
+++ dir1/script1.py
@@ -0,0 +1,8 @@
+# example of relative path reference
+f = open('../settings.txt')
+
+# you can use both absolute or relative paths
+# to add or remove a file
+
+# however, an absolute path is more reliable
+# than a relative path 
